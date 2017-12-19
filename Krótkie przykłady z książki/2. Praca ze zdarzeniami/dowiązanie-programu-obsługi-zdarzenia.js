f52252807fdf5408b2b0af06c7047535ccdc1f91
Ewa <ewkak95@gmial.com>
2017-12-20 00:01:23 +0100

    zamiena z funkci wstawionej do instrukcji zdarzenia na funkcję anonimową

--- Krótkie przykłady z książki/2. Praca ze zdarzeniami/dowiązanie-programu-obsługi-zdarzenia.js
+++ Krótkie przykłady z książki/2. Praca ze zdarzeniami/dowiązanie-programu-obsługi-zdarzenia.js
@@ -1,7 +1,16 @@
-$(document).ready(function() {
-    $('.mylink.bind').on("click", alertMe());
+//Ten kod uruchamia sie od razu po załadowaniu DOM'u, zdarzenie "click" nie działa odpowiednio
+// $(document).ready(function() {
+//     $('.mylink').on("click", alertMe());
+//
+//     function alertMe() {
+//         alert("To nie jest wcale ciekawe")
+//     }
+// })
 
-    function alertMe() {
-        alert("To nie jest wcale ciekawe")
-    }
-})
+//to się uruchamia prawidłowo - po kliknięciu w wskazany element 
+//użyta funkcja anonimowa
+$(document).ready(function(){
+    $('.mylink').on('click', function() {
+        alert("Jeszcze więcej nudy");
+    });
+});
